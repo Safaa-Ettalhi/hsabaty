f9492f0f5a8667323190305e8d4cba1ff4998f29
Safaa <Safaeettalhi@gmail.com>
2026-02-21 18:16:51 +0000

    page chat avec ChatPanel

---
+++ frontend/src/app/(dashboard)/chat/page.tsx
@@ -0,0 +1,9 @@
+import { ChatPanel } from "@/components/chat-panel"
+
+export default function ChatPage() {
+  return (
+    <div className="flex flex-1 flex-col min-h-0">
+      <ChatPanel />
+    </div>
+  )
+}
