2696e1d8c7120278bcf57891c9b52df092f1cfdf
Safaa <Safaeettalhi@gmail.com>
2026-03-16 00:05:36 +0000

    Ajout de l'exportation des services VectorService, EmailService, ExportService et JWTService

--- backend/src/services/index.ts
+++ backend/src/services/index.ts
@@ -1,1 +1,5 @@
 export { CronService } from './cronService';
+export { VectorService } from './vectorService';
+export { EmailService } from './emailService';
+export { ExportService } from './exportService';
+export { JWTService } from './jwtService';
