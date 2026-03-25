86d6d8c26477b1f3b643c1f8fe29155e9b9e1f9e
Safaa <Safaeettalhi@gmail.com>
2026-03-25 09:57:51 +0100

    Mise à jour de la configuration du projet : suppression des dépendances TypeScript, ajustement des scripts et mise à jour des fichiers de configuration pour utiliser JavaScript

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,9 +1,5 @@
 module.exports = {
-  preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>/src'],
-  testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
-  },
+  testMatch: ['**/__tests__/**/*.test.js', '**/?(*.)+(spec|test).js'],
 };
